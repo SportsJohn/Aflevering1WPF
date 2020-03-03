82ed8c730eefe4c7eb0461bd710311a29ec1be09
Nikolaj <nikolaj.h@live.dk>
2020-03-03 11:48:01 +0100

    Aflevering1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aflevering1WPF
+{
+ 
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<User> users;
+        private User p1;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            p1 = new User("Bo", 55, 30,10);
+
+            users = new ObservableCollection<User>() {
+               p1,
+                new User("John", 34, 70, 108),
+                new User("Ib", 11, 320, 102),
+            };
+            listBox.ItemsSource = users;
+            gridOuter.DataContext = users;
+
+        }
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+
+            Microsoft.Win32.OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+                string sFileName = openFileDialog.FileName;
+                string line;
+
+                System.IO.StreamReader file =
+        new System.IO.StreamReader(sFileName);
+
+                while ((line = file.ReadLine()) != null)
+
+                {
+                    var splitLine = line.Split(';');
+                    users.Add(new User(splitLine[0], int.Parse(splitLine[1]), int.Parse(splitLine[2]), int.Parse(splitLine[3])));
+                }
+                listBox.ItemsSource = users;
+
+                ListCount.Content = listBox.Items.Count;
+                LastUpdate.Content = "Last update:" + DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
+                file.Close();
+            }
+            {
+
+            }
+        }
+
+        private void updateList_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Refresh();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aflevering1WPF
+{
+    class User : INotifyPropertyChanged
+    {
+
+        public User(string name, int age, int id, int score) { Name = name; Age = age; Id = id; Score = score;}
+
+        private string name;
+        public string Name
+        {
+            set
+            {
+                name = value;
+                NotifyPropertyChanged(nameof(Name));
+            }
+            get { return name; }
+        }
+        private int age;
+        public int Age
+        {
+            set
+            {
+                age = value;
+                NotifyPropertyChanged(nameof(Age));
+            }
+            get { return age; }
+        }
+
+        private int id;
+
+        public int Id
+        {
+        set
+            {
+                id = value;
+                NotifyPropertyChanged(nameof(id));
+            }
+            get { return id; }
+        }
+
+
+        private int score;
+        public int Score
+        {
+            set
+            {
+                score = value;
+                NotifyPropertyChanged(nameof(Score));
+            }
+            get { return score; }
+        }
+
+        public string ListBoxToString
+        {
+            get
+            {
+                return Name + " " + Id;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
